6284cd03efd10f773dfe3730e518db692af5f33f
gweseloh <greg.weseloh@gmail.com>
2025-03-11 14:24:09 -0500

    Filled arrays

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+let greetings = Array(7).fill("Hello");
+console.log(greetings);
 
+greetings.fill("Hi", 2, 5);
+console.log(greetings);
+
+let multiplesOfTen = Array(5);
+for(let i = 0; i < 5; i++) {
+    multiplesOfTen[i] = i * 10;
+}
+console.log(multiplesOfTen);
